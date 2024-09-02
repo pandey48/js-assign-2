92749f0911e6cbc2714ab545dd45bbefe91d11f7
puspendra kumar paandey <90213390+pandey48@users.noreply.github.com>
2024-09-02 13:43:39 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,66 @@
+/**Qustion no -1 */
+
+let a= 43;
+let b= 32;
+
+let c=a+b;
+console.log('sum of two number = '+c)
+
+/**Qustiom no -2 */
+let x=45;
+let y=45;
+let z=(x<y && x==y);
+console.log(z);
+
+/**Qustiom no -3 */
+let num1 =30;
+let num2 =45;
+if(((num1%10==0) &&(num2%10==0) )){
+    console.log(` true`);
+}
+else if (((num1%10==0) ||(num2%10==0) )) {
+    console.log(`true`)
+    
+} else {
+    console.log('false')
+    
+}
+/**Qustiom no -4 */
+let p = 3088;
+let q = 3088 / 1000;
+console.log(`First digit of 4 digit number is : ${Math.floor(q)}`);
+
+/** Qustion no 5 */
+let digit = 45756;
+let lastDigit = 45756 % 10;
+console.log(`Last digit of 4 digit number is : ${lastDigit}`);
+
+
+
+/**Qustion no -6 */
+let num3 =67;
+let num4 =4356;
+let num5= (num4%num3)
+console.log(num5)
+
+/**qustion no -7 */
+let num6=65;
+let num7=45;
+let num8=(num7*num6)
+console.log(num8);
+/**Qustion  no -8 */
+
+let sub1 =65;
+let sub2 =97;
+let sub3 =76;
+let totalmarks =(sub1+sub2+sub3)
+let average = totalmarks/3;
+console.log( `total marks = ${totalmarks}  `);
+console.log( `average = ${average}`);
+
+/** Qustion no -9 */
+
+
+
+
+
